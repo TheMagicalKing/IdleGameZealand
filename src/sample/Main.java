a8f212fb40de261c1dea9c87dec013d627a51ef4
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2021-05-06 21:58:06 +0200

    næsten færdigt projekt med Jar Fil

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,9 +14,6 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
-
-
-
     }
 
 
